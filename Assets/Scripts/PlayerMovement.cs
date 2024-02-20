9455ad501f75e4aff3876c478ccbf1720ffedf00
DevilishhSmile <manug.diazg@gmail.com>
2024-02-20 15:28:30 -0500

    Anims
    Idle, run and animator setup in player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,55 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
+    private float dirX = 0f;
+    private SpriteRenderer sprite;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 10f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        //Movimiento del psj
+        dirX = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        //Salto
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector3(rb.velocity.x,14f);
+            rb.velocity = new Vector3(rb.velocity.x,jumpForce);
         }
 
+        UpdateAnimationState();
 
+ 
+    }
+        // Ejecutar animaciones cuando se mueve
+    private void UpdateAnimationState()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
     }
 }
